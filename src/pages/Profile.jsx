95946bfe8acebea2d3d90555e60b8ac9cab0bf16
omer <omertopak1@gmail.com>
2024-01-25 03:26:43 +0000

    up

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
     const {userInfo} = useSelector((state)=>state.auth)
-    console.log(userInfo);
+    // console.log(userInfo);
     const { logout } = useAuthCall()
   const navigate = useNavigate()
 
